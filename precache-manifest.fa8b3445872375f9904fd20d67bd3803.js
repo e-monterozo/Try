784f1d9048d7acb6aaad15141bd0160e7b3939db
e-monterozo <e.monterozo23@gmail.com>
2019-09-28 10:47:18 +0800

    Updates

---
+++ precache-manifest.fa8b3445872375f9904fd20d67bd3803.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dacfa56d4523e7023d915986f0be7ec",
+    "url": "/Try/index.html"
+  },
+  {
+    "revision": "4cde1db32534c8609646",
+    "url": "/Try/static/css/main.509900d1.chunk.css"
+  },
+  {
+    "revision": "a2d444f8603019bd1d84",
+    "url": "/Try/static/js/2.1fc2ad01.chunk.js"
+  },
+  {
+    "revision": "4cde1db32534c8609646",
+    "url": "/Try/static/js/main.227f4d2c.chunk.js"
+  },
+  {
+    "revision": "a844affbb0864761e5a1",
+    "url": "/Try/static/js/runtime-main.b4145798.js"
+  }
+]);
